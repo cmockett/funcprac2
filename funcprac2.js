8d537c096047749da56606f56e851a2b70559de1
Student <student@RU-109.local>
2015-09-15 16:30:30 -0600

    3rd

--- funcprac2.js
+++ funcprac2.js
@@ -17,14 +17,11 @@
 // }
 
 // var keyValue = function(a, b){
-// 	var obj = {
-// 		a: b,
-// 	}
-	
+// 	var obj = {};
+// 		obj[a]= b;
 // 	return obj
 // }
 
-// ================================================COMEBACK TO THIS=====================
 
 // var negativeIndex = function(arr, b){
 
@@ -46,4 +43,45 @@
 // 	return str
 // }
 
-var printObject = function(){}
+var printObject = function(a, b){
+	var obj = {a:b}
+		// obj[a]=b
+	
+	return obj;
+}
+// ====================================================COMEBACK TO THIS==============
+
+// var vowels = function(str){
+// 	vowelArray = []
+// 	for(var i=0; i<str.length; i++){
+// 		if (str[i] === "a"){
+// 			vowelArray.push("a")
+// 		}
+// 		else if (str[i] === "e"){
+// 			vowelArray.push("e")
+// 		}
+// 		else if (str[i] === "i"){
+// 			vowelArray.push("i")
+// 		}
+// 		else if (str[i] === "o"){
+// 			vowelArray.push("o")
+// 		}
+// 		else if (str[i] === "u"){
+// 			vowelArray.push("u")
+// 		}
+
+// 	}
+// 	console.log(vowelArray)
+// }
+
+// var twins = function(arr){
+// 	arr = []
+// 	for (var i =0; i<arr.length; i++){
+// 		if (arr[i] === arr[i]-1 || arr[i] === arr[i]+1){
+// 			return true
+// 		}
+// 		else{
+// 		 return false
+// 	}
+// }
+// }
