b9208da63e8aab895703181be1d8b3766cd60e05
Student <student@RU-109.local>
2015-09-15 15:39:50 -0600

    2nd

--- funcprac2.js
+++ funcprac2.js
@@ -8,8 +8,42 @@
 // 	return object.name;
 // }
 
-var totalLetters = function(string){
-	var myArray = [string];
-	var newString = myArray.join("");
-	return newString.length;
-}
+// var totalLetters = function(arr){
+// 	var letters = 0
+// 	for(var i=0; i<arr.length; i++){
+// 		letters += arr[i].length;
+// 	}
+// 	return letters
+// }
+
+// var keyValue = function(a, b){
+// 	var obj = {
+// 		a: b,
+// 	}
+	
+// 	return obj
+// }
+
+// ================================================COMEBACK TO THIS=====================
+
+// var negativeIndex = function(arr, b){
+
+// 	return arr[arr.length + b]
+// }
+
+// var removeM = function(str){
+// 	indexM = [];
+// 	for (var i=0; i<str.length; i++){
+// 		if (str[i] === "M" || str[i] === "m"){
+
+// 		}
+// 	}
+// }
+
+// var removeM = function(str){
+// 	str = str.split("m").join("")
+// 	str = str.split("M").join("")
+// 	return str
+// }
+
+var printObject = function(){}
